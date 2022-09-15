9b629595a4f92dc3a29a64ca8cf59c172a730845
lucenasoft <getugbr@gmail.com>
2022-09-15 10:40:24 -0300

    Update

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+argument = {'argumentos':['--incognito','--disable-blink-features=AutomationControlled','headless','window-size=1920x1080','disable-gpu']}
 usernames = list()  #Aqui sera armazenado todos os usúarios para verificar.
 local = os.getcwd()
 open_txt = open(f'{local}/config/usernames.txt')
@@ -18,18 +19,16 @@ for l in read_usernames:
 def navegador(a):
     path = 'config/driver'
     chrome_options = Options()
-    chrome_options.add_argument('--incognito')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    for x in range(0,len(argument['argumentos'])):
+        chrome_options.add_argument(f'{argument["argumentos"][x]}')
     manager = ChromeDriverManager(path=path)
-    if a == 1:
-        chrome_options.add_argument('headless')
-        chrome_options.add_argument('window-size=1920x1080')
-        chrome_options.add_argument('disable-gpu')
     driver = webdriver.Chrome(service=Service(manager.install()),chrome_options=chrome_options)
     verific(driver)
+    gerar_dados(driver)
 
 #Nesta função ocorre a verificação das contas. 
 def verific(driver):
+    global narnia1
     for x in range(len(usernames)):
         driver.get(f'https://www.instagram.com/{usernames[x]}')
         sleep(3)
@@ -38,7 +37,15 @@ def verific(driver):
             print('Perfil OK!')
         except:
             print('Perfil inválido')
-        
+            
+
+def gerar_dados(driver):
+    try:
+        inf_dados = driver.find_element(By.CLASS_NAME, '_aa_7').text
+        print(inf_dados)
+    except:
+        print('Sem dados para ler')
+
 navegador(a=1)
 
 """ 
